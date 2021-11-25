22bf05c11cf3fab8c730af6ff470e0a692fd5cea
layer1 <wnfh1004@gmail.com>
2021-11-25 10:34:26 +0000

    method 수정

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, request
 import clss
 app = Flask(__name__)
 host = '0.0.0.0'
-port = 8080
+port = 5000
 
 @app.route('/')
 def hello():
     return "<h1>hello</h1>"
 
-@app.route("/post",methods=['POST'])
+@app.route("/classification",methods=['POST'])
 def post():
     value = request.form['imgurl']
     position = clss.finder(value)
